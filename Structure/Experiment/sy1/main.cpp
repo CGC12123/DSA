634b79276f73e03d9f27cdf0820f3a176de66f2d
CGC12123 <caiguoci@126.com>
2023-04-23 21:19:45 +0800

    更新格式

--- Structure/Experiment/sy1/main.cpp
+++ Structure/Experiment/sy1/main.cpp
@@ -3,14 +3,12 @@
 #include "head.h"
 using namespace std;
 
-
-
 int main()
 {
     int A[7] = {5, 2, 7, 4, 6, 3, 1};
     int j;
     cout << "origin nums: " << endl;
-    for (j = 0; j < 7; j ++)
+    for (j = 0; j < 7; j++)
     {
         cout << A[j] << ",";
     }
@@ -18,7 +16,7 @@ int main()
     int n = 7;
     bubble(A, n);
     cout << "sorted: " << endl;
-    for (j = 0; j < 7; j ++)
+    for (j = 0; j < 7; j++)
     {
         cout << A[j] << ",";
     }
