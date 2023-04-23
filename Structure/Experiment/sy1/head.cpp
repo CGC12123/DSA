1b2aec9d5989e057b9b705e423d581022bac9d50
CGC12123 <caiguoci@126.com>
2023-04-23 21:22:46 +0800

    更新格式

--- Structure/Experiment/sy1/head.cpp
+++ Structure/Experiment/sy1/head.cpp
@@ -1,4 +1,4 @@
-#include"head.h"
+#include "head.h"
 
 void bubble(int A[], int n)
 {
@@ -8,17 +8,17 @@ void bubble(int A[], int n)
     {
         sorted = true;
         int i;
-        for(i = 0; i < n - 1; i ++)
+        for (i = 0; i < n - 1; i++)
         {
-            num_scan ++;
-            if(A[i] > A[i + 1])
+            num_scan++;
+            if (A[i] > A[i + 1])
             {
-                num_change ++;
-                swap(A[i] , A[i + 1]);
+                num_change++;
+                swap(A[i], A[i + 1]);
                 sorted = false;
             }
         }
-        num_scan ++;
+        num_scan++;
     }
     cout << "num_scan: " << num_scan << endl;
     cout << "num_change: " << num_change << endl;
