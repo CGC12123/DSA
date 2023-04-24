1cc493f3533bacadae341d8f43bb86fdfd45763d
CGC12123 <caiguoci@126.com>
2023-04-24 16:16:53 +0800

    创建实验2模板

---
+++ Experiment/sy2/head.h
@@ -0,0 +1,7 @@
+#ifndef __HEAD_H
+#define __HEAD_H
+#include "iostream"
+using namespace std;
+
+
+#endif
---
+++ Experiment/sy2/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <utility>
+#include "head.h"
+using namespace std;
+
+int main()
+{
+    cout << 66 << endl;
+    return 0;
+}
