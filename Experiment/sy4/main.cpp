af21e15cfc9a96b3b056f746bc2b1bcb6e4d552b
CGC12123 <caiguoci@126.com>
2023-04-29 19:15:13 +0800

    归并排序测试

--- Experiment/sy4/main.cpp
+++ Experiment/sy4/main.cpp
@@ -3,9 +3,11 @@
 int main()
 {
     Vector UnSesrchA(7, -1);
-    int UnSortedVector31[7] = {2, 5, 4, 9, 8, 12, 7};
-    UnSesrchA.add_vector(UnSortedVector31, 0, 7);
-    cout << "array: ";
+    int UnSortedVector31[] = {2, 5, 4, 1, 9, 8, 12, 7};
+    UnSesrchA.add_vector(UnSortedVector31, 0, 8);
+    UnSesrchA.show();
+    
+    UnSesrchA.mergeSort(0, 7);
     UnSesrchA.show();
     return 0;
 }
